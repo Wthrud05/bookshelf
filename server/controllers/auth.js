5b9a6cc5665961e0d08a4786e9929dd838179a80
Wthrud05 <alex.electrik220@gmail.com>
2023-09-15 17:32:38 +0300

    add: auth functions

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,7 +11,9 @@ exports.createUser = async (req, res) => {
     const doesUserExists = await db.query('select * from users where name = $1', [name])
 
     if (doesUserExists.rows[0]) {
-      res.status(409).json('This name already exists')
+      res.status(409).json({
+        message: 'Это имя уже используется',
+      })
     } else {
       const user = await db.query(
         'insert into users (name, password) values ($1, $2) returning *',
@@ -23,6 +25,91 @@ exports.createUser = async (req, res) => {
           id: user.rows[0].user_id,
           name: user.rows[0].name,
         },
+      })
+    }
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500).json({
+      error: error.message,
+    })
+  }
+}
+
+exports.login = async (req, res) => {
+  const {name, password} = req.body
+
+  try {
+    const user = await db.query('select * from users where name = $1', [name])
+
+    if (!user.rows.length) {
+      res.status(500).json({
+        message: 'Неверное имя пользователя',
+      })
+    } else {
+      brcypt.compare(password, user.rows[0].password, (err, isMatch) => {
+        if (err) throw err
+
+        if (isMatch) {
+          res.status(200).json({
+            message: 'Вход выполнен успешно!',
+            user: {
+              id: user.rows[0].user_id,
+              name: user.rows[0].name,
+            },
+          })
+        } else {
+          res.status(500).json({
+            message: 'Неверный пароль',
+          })
+        }
+      })
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({
+      error: error.message,
+    })
+  }
+}
+
+exports.logout = async (req, res) => {
+  const {name} = req.body
+
+  try {
+    const user = db.query('select * from users where name = $1', [name])
+
+    if (!user.rows.length) {
+      return res.status(500).json({
+        message: 'Пользователь не найден',
+      })
+    } else {
+      return res.status(200).json({
+        message: 'Выход выполнен успешно',
+      })
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({
+      error: error.message,
+    })
+  }
+}
+
+exports.deleteUser = async (req, res) => {
+  const {user_id} = req.body
+
+  try {
+    const user = db.query('delete from users where user_id = $1', [user_id])
+    // const books = db.query('delete from books where user_id = $1', [user_id])
+    // const subs = db.query('delete from subs where user_id = $1', [user_id])
+
+    if (!user.rows.length) {
+      return res.status(500).json({
+        message: 'Пользователь не найден',
+      })
+    } else {
+      return res.status(200).json({
+        message: 'Аккаунт удален',
       })
     }
   } catch (error) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,10 @@
 const {Router} = require('express')
-const {createUser} = require('../controllers/auth')
+const {createUser, login, logout, deleteUser} = require('../controllers/auth')
 const router = new Router()
 
 router.post('/register', createUser)
+router.post('/login', login)
+router.post('/logout', logout)
+router.delete('/delete-account', deleteUser)
 
 module.exports = router
