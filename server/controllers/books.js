2f01ddfa9e84dea66bd52e9a5a76cafe6a3057e3
Wthrud05 <alex.electrik220@gmail.com>
2023-09-19 20:31:27 +0300

    add: crud for books

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -12,11 +12,11 @@ exports.getBooks = async (req, res) => {
 }
 
 exports.createBook = async (req, res) => {
-  const {title, author, user_id, cover, read_date} = req.body
+  const {title, author, user_id, cover, read_date, isAudio} = req.body
   try {
     const book = await db.query(
-      'insert into books (title, author, user_id, cover, read_date) values ($1, $2, $3, $4, $5) returning *',
-      [title, author, user_id, cover, read_date],
+      'insert into books (title, author, user_id, cover, read_date, isAudio) values ($1, $2, $3, $4, $5, $6) returning *',
+      [title, author, user_id, cover, read_date, isAudio],
     )
     console.log(book.rows[0])
     res.status(200).json({
@@ -27,6 +27,43 @@ exports.createBook = async (req, res) => {
     console.log(error.message)
     return res.status(500).json({
       message: 'Что-то пошло не так',
+    })
+  }
+}
+
+exports.upadteBook = async (req, res) => {
+  const {book_id, title, author, cover, read_date} = req.body
+
+  try {
+    const book = await db.query(
+      'update books set title = $1, author = $2, cover = $3, read_date = $4 where book_id = $5 returning *',
+      [title, author, cover, read_date, book_id],
+    )
+    res.status(200).json({
+      message: `Книга ${book.rows[0].title} изменена`,
+      book: book.rows[0],
+    })
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500).json({
+      error: 'Что-то пошло не так',
+    })
+  }
+}
+
+exports.deleteBook = async (req, res) => {
+  const {id} = req.body
+
+  try {
+    const book = await db.query('delete from books where book_id = $1', [id])
+    console.log(book.rows)
+    res.status(200).json({
+      message: `Книга удалена`,
+    })
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500).json({
+      error: 'Что-то пошло не так',
     })
   }
 }
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,8 +1,10 @@
 const {Router} = require('express')
-const {getBooks, createBook} = require('../controllers/books')
+const {getBooks, createBook, upadteBook, deleteBook, addColums} = require('../controllers/books')
 const router = new Router()
 
 router.get('/books', getBooks)
 router.post('/books-create', createBook)
+router.put('/books', upadteBook)
+router.delete('/books', deleteBook)
 
 module.exports = router
