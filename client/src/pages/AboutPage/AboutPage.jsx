c8f1e2b6454cd2a992c61c411f0e517675a28fc2
Wthrud05 <alex.electrik220@gmail.com>
2023-12-25 15:49:51 +0300

    upd: about page

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,12 +1,15 @@
 import React, {useRef} from 'react'
 import styles from './AboutPage.module.scss'
-import {motion, useInView} from 'framer-motion'
-import title from '../../assets/aboutpage-title.svg'
+import {motion} from 'framer-motion'
+import logo from '../../assets/Logo-white.svg'
 import bs from '../../assets/bs.svg'
 import addBook from '../../assets/add_book.svg'
 import updBook from '../../assets/upd-book.svg'
 import sub from '../../assets/sub.svg'
 import share from '../../assets/share.svg'
+import booksV from '../../assets/books-v.svg'
+import booksH from '../../assets/books-h.svg'
+import {NavLink} from 'react-router-dom'
 
 const About = () => {
   return (
@@ -22,8 +25,8 @@ const About = () => {
           initial={{opacity: 0, y: '-50px'}}
           whileInView={{opacity: 1, y: 0}}
           transition={{duration: 1}}
-          src={title}
-          alt="title"
+          src={logo}
+          alt="logo"
         />
         <motion.p
           initial={{opacity: 0, y: '100px'}}
@@ -35,51 +38,91 @@ const About = () => {
         </motion.p>
       </div>
       <div className={styles.SecondScreen}>
+        <img className={styles.BooksV} src={booksV} alt="books" />
+        <img className={styles.BooksV} src={booksV} alt="books" />
+        <img className={styles.BooksH} src={booksH} alt="books" />
+        <img className={styles.BooksH} src={booksH} alt="books" />
+
+        <div className={styles.Header}>
+          <motion.img
+            initial={{opacity: 0, x: '-200px'}}
+            whileInView={{opacity: 1, x: 0}}
+            transition={{duration: 0.8}}
+            src={bs}
+            alt="bs"
+          />
+          <motion.h1
+            initial={{opacity: 0, y: '100px'}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{duration: 0.5}}
+          >
+            Создайте свою электронную книжную полку!
+          </motion.h1>
+          <motion.img
+            initial={{opacity: 0, x: '200px'}}
+            whileInView={{opacity: 1, x: 0}}
+            transition={{duration: 0.8}}
+            src={bs}
+            alt="bs"
+          />
+        </div>
+
         <motion.div
-          initial={{opacity: 0, y: '100px'}}
+          initial={{opacity: 0, y: '150px'}}
           whileInView={{opacity: 1, y: 0}}
           transition={{duration: 0.5}}
-          className={styles.Header}
+          className={styles.Section}
         >
-          <img src={bs} alt="bs" />
-          <h1>Создайте свою электронную книжную полку!</h1>
-        </motion.div>
-        <div className={styles.Section}>
-          <motion.div
-            initial={{opacity: 0, x: '-100%'}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.8}}
-          >
+          <div>
             <img src={addBook} alt="add_book" />
-            <h3>Добавляйте прочитанные книги в колеекцию</h3>
-          </motion.div>
-          <motion.div
-            initial={{opacity: 0, x: '100%'}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.8}}
-          >
+            <h3>Добавляйте прочитанные книги в коллекцию</h3>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{opacity: 0, y: '150px'}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.6}}
+          className={styles.Section}
+        >
+          <div>
             <img src={updBook} alt="upd_book" />
             <h3>Редактируйте и удаляйте книги в вашей коллекции</h3>
-          </motion.div>
-        </div>
-        <div className={styles.Section}>
-          <motion.div
-            initial={{opacity: 0, x: '-100%'}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 1}}
-          >
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{opacity: 0, y: '150px'}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.7}}
+          className={styles.Section}
+        >
+          <div>
             <img src={sub} alt="sub" />
             <h3>Подпишитесь на своих друзей и следите за их обновлениями</h3>
-          </motion.div>
-          <motion.div
-            initial={{opacity: 0, x: '100%'}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 1}}
-          >
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{opacity: 0, y: '150px'}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: 0.8}}
+          className={styles.Section}
+        >
+          <div>
             <img src={share} alt="share" />
             <h3>Делитесь своими успехами с друзьями</h3>
-          </motion.div>
-        </div>
+          </div>
+        </motion.div>
+
+        <motion.button
+          initial={{rotate: 0, scale: 1}}
+          whileHover={{scale: 1.05}}
+          whileTap={{scale: 0.95}}
+          transition={{duration: 0.05}}
+        >
+          <NavLink to={'/'}>Начать</NavLink>
+        </motion.button>
       </div>
     </motion.div>
   )
